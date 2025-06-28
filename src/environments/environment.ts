faebadef06470124c978268d4e4a603939748880
Adrián Garrido <adriangbustos8@gmail.com>
2025-06-28 18:38:33 -0500

    Delete the 'my_old_folder' directory

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyC0GIlssVm0jsFRaRLyrzmwAS3WSrcJ5so",
-  authDomain: "verduleate-4e5c0.firebaseapp.com",
-  projectId: "verduleate-4e5c0",
-  storageBucket: "verduleate-4e5c0.firebasestorage.app",
-  messagingSenderId: "337862343335",
-  appId: "1:337862343335:web:82ec306acef5c4fb5e7982"
-}
